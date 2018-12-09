b3d69202b3feeea1915677c03136c9bde56b9dd7
autodocs <autodocs>
2018-12-09 10:18:09 +0000

    build based on 4c695eb

---
+++ latest/search_index.js
@@ -0,0 +1,123 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Base.delete!-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "Base.delete!",
+    "category": "method",
+    "text": "Delete current node and return the previous node.\n\nWarning: removed nodes are not reclaimed from memory for simplicity reasons\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#Base.insert!-Tuple{CircularList.Buffer,Any}",
+    "page": "Home",
+    "title": "Base.insert!",
+    "category": "method",
+    "text": "Insert a new node after the current node and return the new node.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#Base.length-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "Base.length",
+    "category": "method",
+    "text": "Returns the length of the circular list\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.backward!-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "CircularList.backward!",
+    "category": "method",
+    "text": "Shift the current pointer backward.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.circularlist-Union{Tuple{T}, Tuple{T}} where T",
+    "page": "Home",
+    "title": "CircularList.circularlist",
+    "category": "method",
+    "text": "Create a circular list with the specified data element.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.current-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "CircularList.current",
+    "category": "method",
+    "text": "Current node\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.forward!-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "CircularList.forward!",
+    "category": "method",
+    "text": "Shift the current pointer forward.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.next-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "CircularList.next",
+    "category": "method",
+    "text": "Next node\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.previous-Tuple{CircularList.Buffer}",
+    "page": "Home",
+    "title": "CircularList.previous",
+    "category": "method",
+    "text": "Previous node\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.shift!-Tuple{CircularList.Buffer,Int64,Symbol}",
+    "page": "Home",
+    "title": "CircularList.shift!",
+    "category": "method",
+    "text": "Shift the current pointer forward or backward.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.Buffer",
+    "page": "Home",
+    "title": "CircularList.Buffer",
+    "category": "type",
+    "text": "Buffer is used to hold a pre-allocated vector of nodes.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.Node",
+    "page": "Home",
+    "title": "CircularList.Node",
+    "category": "type",
+    "text": "Double linked list\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.allocate!-Tuple{CircularList.Buffer,DataType}",
+    "page": "Home",
+    "title": "CircularList.allocate!",
+    "category": "method",
+    "text": "Allocates a new uninitialized node in the circular list\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.jl-1",
+    "page": "Home",
+    "title": "CircularList.jl",
+    "category": "section",
+    "text": "Modules = [CircularList]"
+},
+
+]}
