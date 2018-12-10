42ece825b11362117d6cd6582d144e0ed4d14ae2
autodocs <autodocs>
2018-12-10 00:22:53 +0000

    build based on 5ac0bb5

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,7 +9,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Base.delete!-Tuple{CircularList.Buffer}",
+    "location": "index.html#Base.delete!-Tuple{CircularList.List}",
     "page": "Home",
     "title": "Base.delete!",
     "category": "method",
@@ -17,7 +17,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Base.insert!-Tuple{CircularList.Buffer,Any}",
+    "location": "index.html#Base.eltype-Tuple{CircularList.List}",
+    "page": "Home",
+    "title": "Base.eltype",
+    "category": "method",
+    "text": "Return the element type of the list\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#Base.insert!-Tuple{CircularList.List,Any}",
     "page": "Home",
     "title": "Base.insert!",
     "category": "method",
@@ -25,7 +33,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Base.length-Tuple{CircularList.Buffer}",
+    "location": "index.html#Base.iterate",
+    "page": "Home",
+    "title": "Base.iterate",
+    "category": "function",
+    "text": "Iteration protocol implementation.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#Base.length-Tuple{CircularList.List}",
     "page": "Home",
     "title": "Base.length",
     "category": "method",
@@ -33,11 +49,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#CircularList.backward!-Tuple{CircularList.Buffer}",
+    "location": "index.html#Base.show-Union{Tuple{T}, Tuple{IO,List{T}}} where T",
+    "page": "Home",
+    "title": "Base.show",
+    "category": "method",
+    "text": "Show list.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#Base.size-Tuple{CircularList.List}",
+    "page": "Home",
+    "title": "Base.size",
+    "category": "method",
+    "text": "Return the size of the list.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.backward!-Tuple{CircularList.List}",
     "page": "Home",
     "title": "CircularList.backward!",
     "category": "method",
     "text": "Shift the current pointer backward.\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.circularlist-Union{Tuple{AbstractArray{T,1}}, Tuple{T}} where T",
+    "page": "Home",
+    "title": "CircularList.circularlist",
+    "category": "method",
+    "text": "Create a circular list from any vector\n\n\n\n\n\n"
 },
 
 {
@@ -49,15 +89,15 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#CircularList.current-Tuple{CircularList.Buffer}",
+    "location": "index.html#CircularList.current-Tuple{CircularList.List}",
     "page": "Home",
     "title": "CircularList.current",
     "category": "method",
-    "text": "Current node\n\n\n\n\n\n"
+    "text": "Return the current node.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#CircularList.forward!-Tuple{CircularList.Buffer}",
+    "location": "index.html#CircularList.forward!-Tuple{CircularList.List}",
     "page": "Home",
     "title": "CircularList.forward!",
     "category": "method",
@@ -65,23 +105,23 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#CircularList.next-Tuple{CircularList.Buffer}",
+    "location": "index.html#CircularList.next-Tuple{CircularList.List}",
     "page": "Home",
     "title": "CircularList.next",
     "category": "method",
-    "text": "Next node\n\n\n\n\n\n"
+    "text": "Return the ext node.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#CircularList.previous-Tuple{CircularList.Buffer}",
+    "location": "index.html#CircularList.previous-Tuple{CircularList.List}",
     "page": "Home",
     "title": "CircularList.previous",
     "category": "method",
-    "text": "Previous node\n\n\n\n\n\n"
+    "text": "Return the previous node.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#CircularList.shift!-Tuple{CircularList.Buffer,Int64,Symbol}",
+    "location": "index.html#CircularList.shift!-Tuple{CircularList.List,Int64,Symbol}",
     "page": "Home",
     "title": "CircularList.shift!",
     "category": "method",
@@ -89,11 +129,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#CircularList.Buffer",
+    "location": "index.html#CircularList.List",
     "page": "Home",
-    "title": "CircularList.Buffer",
+    "title": "CircularList.List",
     "category": "type",
-    "text": "Buffer is used to hold a pre-allocated vector of nodes.\n\n\n\n\n\n"
+    "text": "List is used to hold a pre-allocated vector of nodes.\n\n\n\n\n\n"
 },
 
 {
@@ -101,15 +141,31 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "CircularList.Node",
     "category": "type",
-    "text": "Double linked list\n\n\n\n\n\n"
+    "text": "Doubly linked list implementation\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#CircularList.allocate!-Tuple{CircularList.Buffer,DataType}",
+    "location": "index.html#CircularList.allocate!-Tuple{CircularList.List,DataType}",
     "page": "Home",
     "title": "CircularList.allocate!",
     "category": "method",
     "text": "Allocates a new uninitialized node in the circular list\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.head-Tuple{CircularList.List}",
+    "page": "Home",
+    "title": "CircularList.head",
+    "category": "method",
+    "text": "Return the head of the list (current node).\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#CircularList.tail-Tuple{CircularList.List}",
+    "page": "Home",
+    "title": "CircularList.tail",
+    "category": "method",
+    "text": "Return the tail of the list\n\n\n\n\n\n"
 },
 
 {
