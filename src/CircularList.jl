93d3d07a3f56d66e9bf05d6b205daab5e7b899fd
ShaoWei Teo <tookunokaze@gmail.com>
2022-12-24 21:17:32 +0800

    Removed extra whitespaces and newlines

--- src/CircularList.jl
+++ src/CircularList.jl
@@ -2,15 +2,15 @@ module CircularList
 
 import Base: insert!, delete!, length, size, eltype, iterate, show
 
-export circularlist, length, size, current, previous, next, 
+export circularlist, length, size, current, previous, next,
     insert!, delete!, shift!, forward!, backward!, jump!,
     eltype, iterate, show, head, tail
 
 """
 Doubly linked list implementation
 """
-mutable struct Node{T} 
-    data::Union{T, Nothing} 
+mutable struct Node{T}
+    data::Union{T, Nothing}
     prev::Union{Node{T}, Nothing}
     next::Union{Node{T}, Nothing}
 end
@@ -18,7 +18,7 @@ end
 """
 List is used to hold a pre-allocated vector of nodes.
 """
-mutable struct List{T} 
+mutable struct List{T}
     nodes::Vector{Node{T}}      # preallocated array of nodes
     current::Node{T}            # current "head" or the circular list
     length::Int                 # number of active elements
@@ -60,9 +60,9 @@ function allocate!(CL::List, T::DataType)
 end
 
 "Insert a new node after the current node and return the new node."
-function insert!(CL::List, data) 
+function insert!(CL::List, data)
     cl = CL.current
-    
+
     n = allocate!(CL, typeof(data))  # make a new node and arrange prev/next pointers
     n.data = data
     n.prev = cl
@@ -70,7 +70,7 @@ function insert!(CL::List, data)
 
     cl.next = n         # fix prev node's next pointer
     n.next.prev = n     # fix next node's prev pointer
-    
+
     CL.current = n     # move pointer to newly inserted node
     return CL
 end
@@ -95,7 +95,7 @@ Shift the current pointer forward or backward.
 """
 function shift!(CL::List, steps::Int, direction::Symbol)
     for i in 1:steps
-        if direction == :forward 
+        if direction == :forward
             CL.current = CL.current.next
         elseif direction == :backward
             CL.current = CL.current.prev
@@ -161,4 +161,3 @@ function show(io::IO, node::Node{T}) where T
 end
 
 end # module
-
